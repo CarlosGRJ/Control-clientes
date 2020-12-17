a319ccbf1bafabcc472007c2b52e971931e36ff2
CarlosGRJ <carlosgrj@outlook.com>
2020-12-16 20:52:43 -0600

    Correccion Clase Conexion

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -5,23 +5,29 @@ import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class Conexion {
+
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/control_clientes?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD = "admin";
-    
+
+    private static BasicDataSource dataSource;
+
     public static DataSource getDataSource() {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setUrl(JDBC_URL);
-        ds.setUsername(JDBC_USER);
-        ds.setPassword(JDBC_PASSWORD);
-        ds.setInitialSize(50);
-        return ds;
+        if (dataSource == null) {
+            dataSource = new BasicDataSource();
+            dataSource.setUrl(JDBC_URL);
+            dataSource.setUsername(JDBC_USER);
+            dataSource.setPassword(JDBC_PASSWORD);
+            dataSource.setInitialSize(50);
+        }
+
+        return dataSource;
     }
-    
+
     public static Connection getConnection() throws SQLException {
         return getDataSource().getConnection();
     }
-    
+
     public static void close(ResultSet rs) {
         try {
             rs.close();
@@ -29,7 +35,7 @@ public class Conexion {
             ex.printStackTrace(System.out);
         }
     }
-    
+
     public static void close(PreparedStatement stmt) {
         try {
             stmt.close();
@@ -37,7 +43,7 @@ public class Conexion {
             ex.printStackTrace(System.out);
         }
     }
-    
+
     public static void close(Connection conn) {
         try {
             conn.close();
